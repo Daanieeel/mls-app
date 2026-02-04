af59c78201da22dc2cccc4b8db61dd0821b994fa
greluap <greluap@gmail.com>
2026-02-04 09:02:03 +0100

    Feature create group endpoints (#3)
    * Added endpoints for Group
    
    * added missing endpoints
    
    ---------
    
    Co-authored-by: Daniel Dopatka <96653085+Daanieeel@users.noreply.github.com>

--- apps/rest/src/modules/group/index.ts
+++ apps/rest/src/modules/group/index.ts
@@ -1,43 +1,104 @@
 import { Elysia, t } from 'elysia';
+import { requireAuth } from '../auth/guard';
 import { GroupModel } from './model';
 import { GroupService } from './service';
 
 export const groupRouter = new Elysia({ prefix: '/groups' })
-  .get('/', () => {
-    GroupService.getAllGroups();
-  })
-  .get('/:id', ({ params }) => {
-    GroupService.getGroup({
-      params: params,
-    });
-  })
+  .use(requireAuth)
   .post(
     '/',
-    ({ body }) => {
-      return GroupService.createGroup({
+    ({ body, user, set }) => {
+      const createdGroup = GroupService.createGroup({
         body: body,
-        id: '1',
+        executorId: user.id,
       });
+      set.status = 202;
+      return createdGroup;
     },
     {
       body: GroupModel.CreateGroupBody,
     },
   )
-  .patch(
-    '/:id',
-    ({ body, params }) => {
-      return GroupService.updateGroup({
+  .post(
+    '/:id/add-user',
+    ({ body, params, set }) => {
+      const updatedGroup = GroupService.addUserToGroup({
         body: body,
         params: params,
       });
+      if (updatedGroup === undefined) {
+        //TODO: catch prisma error P2025
+        set.status = 404;
+      } else {
+        set.status = 202;
+      }
+      return updatedGroup;
     },
     {
-      body: GroupModel.UpdateGroupBody,
-      params: GroupModel.UpdateGroupParams,
+      body: GroupModel.AddUserBody,
+      params: GroupModel.AddUserParams,
     },
   )
-  .delete('/:id', ({ params }) => {
-    return GroupService.deleteGroup({
-      params: params,
+  .post(
+    '/:id/remove-user',
+    ({ body, params, set }) => {
+      const updatedGroup = GroupService.removeUserFromGroup({
+        body: body,
+        params: params,
+      });
+      if (updatedGroup === undefined) {
+        set.status = 404;
+      } else {
+        set.status = 202;
+      }
+      return updatedGroup;
+    },
+    {
+      body: GroupModel.RemoveUserBody,
+      params: GroupModel.RemoveUserParams,
+    },
+  )
+  .post(
+    '/:id/leave',
+    ({ user, params, set }) => {
+      const updatedGroup = GroupService.leaveGroup({
+        executorId: user.id,
+        params: params,
+      });
+      if (updatedGroup === undefined) {
+        set.status = 404;
+      } else {
+        set.status = 202;
+      }
+      return updatedGroup;
+    },
+    {
+      params: GroupModel.LeaveGroupParams,
+    },
+  )
+  .get('/', ({ user, set }) => {
+    const groups = GroupService.getAllGroups({
+      executorId: user.id,
     });
+    set.status = 200;
+    return groups;
+  })
+  .get('/:id', ({ user, params, set }) => {
+    const group = GroupService.getGroupById({
+      executorId: user.id,
+      groupId: params.id,
+    });
+    if (group === undefined) {
+      set.status = 404;
+    } else {
+      set.status = 200;
+    }
+    return group;
+  })
+  .get('/sync', ({ user, set }) => {
+    const groups = GroupService.syncGroups({
+      executorId: user.id,
+    });
+    set.status = 200;
+    return groups;
   });
