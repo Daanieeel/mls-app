ae0bde0bf589bcc52e507745520fc17262154fb8
justintischendorf <154767599+justintischendorf@users.noreply.github.com>
2026-02-04 09:03:42 +0100

    feature: implemented message endpoints (#4)
    * implemented endpoint for route messages
    
    * updated endpoints, set comments
    
    ---------
    
    Co-authored-by: Justin Tischendorf <justin.tischendorf@huk-coburg.de>
    Co-authored-by: Daniel Dopatka <96653085+Daanieeel@users.noreply.github.com>

--- apps/rest/src/modules/message/index.ts
+++ apps/rest/src/modules/message/index.ts
@@ -1,12 +1,55 @@
 import { Elysia } from 'elysia';
 import { kafkaPlugin } from '../../utils/kafka';
+import { requireAuth } from '../auth/guard';
 import { MessageModel } from './model';
 import { MessageService } from './service';
 
-export const messageRouter = new Elysia({ prefix: '/messages' }).use(kafkaPlugin()).post(
-  '/',
-  ({ body }) => {
-    MessageService.createMessage({ body: body });
-  },
-  { body: MessageModel.CreateMessageBody },
-);
+export const messageRouter = new Elysia({ prefix: '/messages' })
+  .use(kafkaPlugin())
+  .use(requireAuth)
+  .post(
+    '/',
+    ({ body, user, set }) => {
+      set.status = 202;
+      const createdMessage = MessageService.createMessage({ body, userId: user.id });
+      return createdMessage;
+    },
+    { body: MessageModel.CreateMessageBody },
+  )
+
+  .use(requireAuth)
+  .patch(
+    '/:messageId',
+    ({ body, params, user, set }) => {
+      const updatedMessage = MessageService.updateMessage({ body, params, userId: user.id });
+      // TODO: Error() => undefined
+      if (updatedMessage === undefined) {
+        set.status = 404;
+      } else {
+        set.status = 202;
+      }
+      return updatedMessage;
+    },
+    {
+      body: MessageModel.UpdateMessageBody,
+    },
+  )
+
+  .delete(
+    '/:messageId',
+    ({ params, set }) => {
+      const deletedMessage = MessageService.deleteMessage({
+        params: params,
+      });
+      // TODO: Error() => undefined
+      if (deletedMessage === undefined) {
+        set.status = 404;
+      } else {
+        set.status = 202;
+      }
+      return deletedMessage;
+    },
+    {
+      params: MessageModel.DeleteMessageParams,
+    },
+  );
--- apps/rest/src/modules/message/service.ts
+++ apps/rest/src/modules/message/service.ts
@@ -1,5 +1,57 @@
+import { prisma } from '@repo/database';
 import type { MessageModel } from './model';
 
 export abstract class MessageService {
-  static createMessage({ body }: { body: (typeof MessageModel.CreateMessageBody)['static'] }) {}
+  static createMessage({
+    body,
+    userId,
+  }: {
+    body: typeof MessageModel.CreateMessageBody['static'];
+    userId: string;
+  }) {
+    return prisma.globalMessage.create({
+      data: {
+        ...body,
+        sender: {
+          connect: {
+            id: userId,
+          },
+        },
+        payload: Buffer.from(body.payload),
+      },
+    });
+  }
+
+  static updateMessage({
+    body,
+    params,
+    userId,
+  }: {
+    body: typeof MessageModel.UpdateMessageBody['static'];
+    params: typeof MessageModel.UpdateMessageParams['static'];
+    userId: string;
+  }) {
+    return prisma.globalMessage.update({
+      where: {
+        id: params.messageId,
+      },
+      data: {
+        ...body,
+        sender: {
+          connect: {
+            id: userId,
+          },
+        },
+        payload: Buffer.from(body.payload),
+      },
+    });
+  }
+
+  static deleteMessage({ params }: { params: typeof MessageModel.DeleteMessageParams['static'] }) {
+    return prisma.globalMessage.delete({
+      where: {
+        ...params,
+      },
+    });
+  }
 }
