34241194658b83e594f00fbe25e9d189b0668303
greluap <greluap@gmail.com>
2026-02-05 15:35:48 +0100

    added endpoint for add user to group

--- apps/rest/src/modules/group/index.ts
+++ apps/rest/src/modules/group/index.ts
@@ -4,10 +4,8 @@ import { kafkaPlugin } from '../../utils/kafka';
 import { GroupModel } from './model';
 import { GroupService } from './service';
 import { KAFKA_TOPIC_TYPES, type MinimalCloudEvent } from '@repo/utils';
-import { kafkaPlugin } from '../../utils/kafka';
 
 export const groupRouter = new Elysia({ prefix: '/groups' })
-  .use(kafkaPlugin())
   .use(requireAuth)
   .use(kafkaPlugin())
   .post(
@@ -38,18 +36,29 @@ export const groupRouter = new Elysia({ prefix: '/groups' })
 
   .post(
     '/:id/add-user',
-    ({ body, params, set }) => {
-      const updatedGroup = GroupService.addUserToGroup({
+    async ({ body, params, set, producer }) => {
+      const createdCloudEvent: MinimalCloudEvent = await GroupService.addUserToGroup({
         body: body,
         params: params,
       });
-      if (updatedGroup === undefined) {
+
+      producer.send({
+        topic: KAFKA_TOPIC_TYPES.GROUP,
+        messages: [
+          {
+            key: createdCloudEvent.subject,
+            value: JSON.stringify(createdCloudEvent),
+          },
+        ],
+      });
+
+      if (createdCloudEvent === undefined) {
         //TODO: catch prisma error P2025
         set.status = 404;
       } else {
         set.status = 202;
       }
-      return updatedGroup;
+      return createdCloudEvent;
     },
     {
       body: GroupModel.AddUserBody,
