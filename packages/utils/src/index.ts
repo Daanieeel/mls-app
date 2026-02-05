6268f36c70acaa833cb7018eef9c9e438feec11a
justintischendorf <154767599+justintischendorf@users.noreply.github.com>
2026-02-05 12:48:56 +0100

    refactor: changed CLOUD_EVENT_TYPES

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -5,7 +5,7 @@ export const CLOUD_EVENT_TYPES = {
   MESSAGE_UPDATED: 'de.messenger.message.updated',
   MESSAGE_DELETED: 'de.messenger.message.deleted',
   MESSAGE_NOTIFICATION_PUBLISHED: 'de.messenger.message.notification.published',
-  GROUP_LEFT: 'de.group.left',
+  GROUP_LEFT: 'de.messenger.group.left',
 } as const;
 
 export const KAFKA_TOPIC_TYPES = {
